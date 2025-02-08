9bd33bb4f569bf99af48df42f3d3f9a78631d614
Josinaldo-Jr <josinaldoeuzebiojr@gmail.com>
2025-02-08 19:32:09 -0300

    feat: add form submission state to form

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -57,9 +57,10 @@ export const YouTubeForm = () => {
   });
 
   const { register, control, handleSubmit, formState, watch, getValues, setValue } = form;
-  const { errors, touchedFields, dirtyFields, isDirty, isValid } = formState;
+  const { errors, /*touchedFields, dirtyFields,*/ isDirty, isValid, isSubmitting, isSubmitted, isSubmitSuccessful, submitCount } = formState;
 
-  console.log({touchedFields, dirtyFields, isDirty, isValid});
+  console.log({isSubmitting, isSubmitted, isSubmitSuccessful, submitCount})
+  // console.log({touchedFields, dirtyFields, isDirty, isValid});
 
   const { fields, append, remove } = useFieldArray({
     name: 'phNumbers',
@@ -236,7 +237,7 @@ export const YouTubeForm = () => {
           <p className="error">{errors.dob?.message}</p>
         </div>
 
-        <button disabled={!isDirty || !isValid}>Submit</button>
+        <button disabled={!isDirty || !isValid || isSubmitting}>Submit</button>
         <button type="button" onClick={handleGetValues}>Get values</button>
         <button type="button" onClick={handleSetValue}>Set value</button>
 
